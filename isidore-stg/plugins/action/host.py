672c8dd90cfd25d1b0e3bb7d821aaf3a989c253b
Chmghost <keithminork@gmail.com>
2023-09-11 14:56:27 -0400

    Changed name module for host and added an action file for host

---
+++ isidore-stg/plugins/action/host.py
@@ -0,0 +1,14 @@
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+
+    def run(self, tmp=None, task_vars=None):
+        if task_vars is None:
+            task_vars = dict()
+
+        # Use the existing module
+        result = self._execute_module(module_name='host', module_args=self._task.args,
+                                      task_vars=task_vars)
+
+        return result
