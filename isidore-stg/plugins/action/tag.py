79e2c70e942687808dba45b29c24cdb32061cee7
Chmghost <keithminork@gmail.com>
2023-09-11 15:10:31 -0400

    Added an action file for tags and changed the tag module file name

---
+++ isidore-stg/plugins/action/tag.py
@@ -0,0 +1,13 @@
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+
+    def run(self, tmp=None, task_vars=None):
+        if task_vars is None:
+            task_vars = dict()
+
+        # Use the addtag module
+        result = self._execute_module(module_name='addtag', module_args=self._task.args, task_vars=task_vars)
+
+        return result
